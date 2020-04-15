a6e83a77be22859ef7ff22eab54c441ef1154a06
Ulysses Kuntze <kuntze@gmail.com>
2020-04-15 13:35:56 +1200

    Updates

---
+++ precache-manifest.1f11e9ff41384ccd60a443c144d0f637.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19dce5af57b4421d356132089d4cc67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e54ab70e2880236010f6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "634aad73b2ad63df3e9a",
+    "url": "/static/js/2.709bc133.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.709bc133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e54ab70e2880236010f6",
+    "url": "/static/js/main.79dd0024.chunk.js"
+  },
+  {
+    "revision": "80492c6dd000356cd7e8",
+    "url": "/static/js/runtime-main.c69d12cd.js"
+  }
+]);
