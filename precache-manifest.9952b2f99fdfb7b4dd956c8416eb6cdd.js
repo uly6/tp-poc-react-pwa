5f19737cdd87c3a1958da921a7fc3e2937c7b906
Ulysses Kuntze <kuntze@gmail.com>
2020-04-16 20:51:24 +1200

    Updates

--- precache-manifest.9952b2f99fdfb7b4dd956c8416eb6cdd.js
+++ precache-manifest.9952b2f99fdfb7b4dd956c8416eb6cdd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a31070e79a3750a5cef39385abfdb2e",
+    "revision": "21e1d33015135339b6fcd36bc2ce6838",
     "url": "/tp-poc-react-pwa/index.html"
   },
   {
-    "revision": "d6558f74cb97f70b0244",
+    "revision": "2b4c541dfa304100b72b",
     "url": "/tp-poc-react-pwa/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "d52138b444accaea863b",
-    "url": "/tp-poc-react-pwa/static/js/2.93183817.chunk.js"
+    "revision": "bbde87da42b426e011b1",
+    "url": "/tp-poc-react-pwa/static/js/2.97b227e4.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/tp-poc-react-pwa/static/js/2.93183817.chunk.js.LICENSE.txt"
+    "url": "/tp-poc-react-pwa/static/js/2.97b227e4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d6558f74cb97f70b0244",
-    "url": "/tp-poc-react-pwa/static/js/main.dea6d26d.chunk.js"
+    "revision": "2b4c541dfa304100b72b",
+    "url": "/tp-poc-react-pwa/static/js/main.5ae44921.chunk.js"
   },
   {
     "revision": "95b28c9042193d6f1da7",
